c48b4a5f4fd620c1c09de21cf31376e723ed727e
whitehatjoker <rkhudoyberdiev@gmail.com>
2020-07-05 03:35:38 +0500

    Add onscroll event to highlight sections

--- js/app.js
+++ js/app.js
@@ -18,7 +18,8 @@
  * 
 */
 const fragment = document.createDocumentFragment();
-
+const navBar = document.getElementById('navbar__list');
+const sections = document.querySelectorAll('main section');
 /**
  * End Global Variables
  * Start Helper Functions
@@ -28,11 +29,33 @@ function buildList(listItem, index) {
     const newList = document.createElement('li');
     const listName = listItem.getAttribute('data-nav');
     const listId = listItem.getAttribute('id');
-    newList.innerHTML = `<a href='#${listId}' data-navlist="${listId}">${listName}</a>`;
+    newList.setAttribute("data-navlist", listId);
+    newList.innerHTML = `<a href='#${listId}'>${listName}</a>`;
     fragment.appendChild(newList);
 }
 
+function addSectionClass(section) {
+    const box = section.getBoundingClientRect();
+    const currentSectionId = section.getAttribute('id');
+    const currentNavLink = document.querySelector(`#navbar__list li[data-navlist="${currentSectionId}"]`);
+    if (box.top <= 750 && box.bottom >= 750) {
+        section.classList.add('active-section');
+        currentNavLink.classList.add('active-link');
+    } else {
+        section.classList.remove('active-section');
+        currentNavLink.classList.remove('active-link');
+    }
+}
 
+function buildScroll(menuLink) {
+    menuLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        console.log('it is running');
+        document.querySelector(menuLink.getAttribute("href")).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+}
 
 /**
  * End Helper Functions
@@ -42,18 +65,21 @@ function buildList(listItem, index) {
 
 // build the nav
 function buildNav() {
-    const sections = document.querySelectorAll('main section');
-    const navBar = document.getElementById('navbar__list');
     sections.forEach(buildList);
     navBar.appendChild(fragment);
 }
 
 
 // Add class 'active' to section when near top of viewport
-
+function makeActive() {
+    sections.forEach(addSectionClass);
+}
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollToAnchor() {
+    const links = document.querySelectorAll('#navbar__list li a');
+    links.forEach(buildScroll);
+} 
 
 /**
  * End Main Functions
@@ -62,12 +88,9 @@ function buildNav() {
 */
 
 // Build menu 
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-    buildNav();
-});
+buildNav();
 // Scroll to section on link click
+scrollToAnchor();
 
 // Set sections as active
-
-
+document.addEventListener('scroll',() => { makeActive();});
